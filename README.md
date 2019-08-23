a9ea54cc6b94d73c391069ff6fcbdb9efbd19619
Lav Patel <lavlp811@gmail.com>
2019-08-23 16:29:47 -0700

    Readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Blog-NodeJS Application
+
+### This application has been created with the help of NodeJS, MongoDB and Express.
+
+## Before running the application make sure the MongoDB server is up and running.
+
+### Run the following commands to make sure the application is running properly:
+```javascript
+npm install
+npm start
+```
+
+### Thank you again! I have deployed the application. You can view it [here](https://blog-lp.herokuapp.com)
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser');
+var dotenv = require('dotenv');
 
 app.use(cookieParser());
 var auth = require('./controllers/user');
@@ -10,10 +11,11 @@ var {requireSignin ,renderHome, create, showSingleBlog, deletePost, updateBlog,u
 
 app.set('view engine', 'ejs');
 app.use('/views', express.static('views'))
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/authentication',{ useNewUrlParser: true }).then(() => {console.log('Database Connected')});
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true }).then(() => {console.log('Database Connected')});
 
 app.get('/',requireSignin,renderHome);
 
